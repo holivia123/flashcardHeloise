d5f9cad00db8e72c4fbfa3460fc7cdc477600eda
holivia123 <heloisegoriano@gmail.com>
2024-11-26 20:30:33 -0300

    Add files via upload

---
+++ flashcard-Heloise/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'Qual é o elemento químico mais abundante no universo?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual planeta do Sistema Solar é conhecido como o Planeta Vermelho?',
+    'Marte'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o processo pelo qual as plantas produzem energia usando luz solar?',
+    'Fotossíntese'
+)
